efb94cb1e32d66eb40629cb0f24dfffebf4104b7
Bola <bolanady00@gmail.com>
2025-01-27 00:13:09 +0200

    add add note state

--- lib/add_note_cubit/cubit/add_note_state.dart
+++ lib/add_note_cubit/cubit/add_note_state.dart
@@ -8,3 +8,15 @@ sealed class AddNoteState extends Equatable {
 }
 
 final class AddNoteInitial extends AddNoteState {}
+final class AddNoteLoading extends AddNoteState {}
+final class AddNoteSuccess extends AddNoteState {}
+final class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+
+  const AddNoteFailure(this.errMessage);
+}
+
+
+
+
+
