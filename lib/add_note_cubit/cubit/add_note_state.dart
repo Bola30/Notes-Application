d2c37c4568ea6456a8870ae5471a30ed01fc9e3a
Bola <bolanady00@gmail.com>
2025-01-27 00:17:02 +0200

    Resolve merge conflicts

--- lib/add_note_cubit/cubit/add_note_state.dart
+++ lib/add_note_cubit/cubit/add_note_state.dart
@@ -8,7 +8,6 @@ sealed class AddNoteState extends Equatable {
 }
 
 final class AddNoteInitial extends AddNoteState {}
-<<<<<<< HEAD
 final class AddNoteLoading extends AddNoteState {}
 final class AddNoteSuccess extends AddNoteState {}
 final class AddNoteFailure extends AddNoteState {
@@ -20,6 +19,3 @@ final class AddNoteFailure extends AddNoteState {
 
 
 
-
-=======
->>>>>>> f6d4d198a22bf56de2f01bb32a947486bbf1a6af
