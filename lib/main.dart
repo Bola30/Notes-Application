a5d396dfd4c949428191fefaebf45d1aaa80cdc3
Bola <bolanady00@gmail.com>
2025-01-31 22:20:11 +0200

    Add Simple Bloc Observer

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_application/add_note_cubit/cubit/add_note_cubit.dart';
 import 'package:notes_application/models/note_model.dart';
+import 'package:notes_application/simple_bloc_observer.dart';
 import 'package:notes_application/views/constans.dart';
 import 'package:notes_application/views/view_notes.dart';
 
 void main() async{
 
     await Hive.initFlutter();
+
+   Bloc.observer = SimpleBlocObserver();
+
     await Hive.openBox(kNotesBox);
     Hive.registerAdapter(NoteModelAdapter());
     
--- lib/views/Add_Note_Bottom_Sheet.dart
+++ lib/views/Add_Note_Bottom_Sheet.dart
@@ -1,11 +1,8 @@
-import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_application/add_note_cubit/cubit/add_note_cubit.dart';
 import 'package:notes_application/views/widgets/Add_Note_Form.dart';
-import 'package:notes_application/views/widgets/Coustem_Button.dart';
-import 'package:notes_application/views/widgets/Coustem_Text_Feild.dart';
 
 class AddNoteBottom extends StatelessWidget {
   const AddNoteBottom({super.key});
@@ -18,15 +15,7 @@ class AddNoteBottom extends StatelessWidget {
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             if (state is AddNoteFailure) {
-              AwesomeDialog(
-                context: context,
-                dialogType: DialogType.warning,
-                animType: AnimType.rightSlide,
-                title: 'Failed',
-                desc: 'Please Try Again',
-                btnCancelOnPress: () {},
-                btnOkOnPress: () {},
-              ).show();
+            print('Failed ${state.errMessage}');
             }
             if (state is AddNoteSuccess) {
               Navigator.pop(context);
